acbe807171897dbadc631bf3e07a48f8edf3f4a7
songjiahao <1109790848@qq.com>
2022-12-02 13:39:11 +0800

    Initial commit

---
+++ models/module.py
@@ -0,0 +1,13 @@
+from models.Models.yolovii import Conv, RobustConv, RobustConv2, GhostConv, RepConv, RepConv_OREPA, DownC,  \
+                 SPP, SPPF, SPPCSPC, GhostSPPCSPC, Focus, Stem, GhostStem,  \
+                 Bottleneck, BottleneckCSPA, BottleneckCSPB, BottleneckCSPC,  \
+                 RepBottleneck, RepBottleneckCSPA, RepBottleneckCSPB, RepBottleneckCSPC,   \
+                 Res, Swin_v2_A, Swin_v2_B, Swin_v2_C, Swin_Transformer_A, Swin_Transformer_B, Swin_Transformer_C, ResCSPA, ResCSPB, ResCSPC,  \
+                 RepRes, RepResCSPA, RepResCSPB, RepResCSPC,  \
+                 ResX, ResXCSPA, ResXCSPB, ResXCSPC,  \
+                 RepResX, RepResXCSPA, RepResXCSPB, RepResXCSPC,  \
+                 Ghost, GhostCSPA, GhostCSPB, GhostCSPC, \
+                 SwinTransformerBlock, \
+                 SwinTransformer2Block
+
+
